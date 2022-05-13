e64a28a3db78d20a62ecd343185ee6109ae8b235
LeviUniAcc <otterbach.levi@gmail.com>
2022-05-13 22:55:10 +0200

    init

---
+++ src/test/java/de/unistuttgart/hackathon/taser/AnalyticsService/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.hackathon.taser.AnalyticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
