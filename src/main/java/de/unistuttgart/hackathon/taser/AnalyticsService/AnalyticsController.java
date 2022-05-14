3c70f5676792d3bbebef929c4dc1c580ac083a06
Max Buchholz <max.buchholz@gmx.de>
2022-05-14 23:05:45 +0200

    Implement calculateStatistics

--- src/main/java/de/unistuttgart/hackathon/taser/AnalyticsService/AnalyticsController.java
+++ src/main/java/de/unistuttgart/hackathon/taser/AnalyticsService/AnalyticsController.java
@@ -1,23 +1,22 @@
 package de.unistuttgart.hackathon.taser.AnalyticsService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-import java.util.LinkedList;
+import javax.annotation.PostConstruct;
 
 @RestController
 public class AnalyticsController {
 
-    /**
-     * Offers the Stats for a specific room
-     *
-     * //TODO: Specify the returned Statistics
-     */
-    @GetMapping("/analytics/{roomIdentifier}")
-    public Collection<String> getStatsForRoom(@PathVariable final String roomIdentifier){
-        return new LinkedList<>();
+    private AnalyticsService analyticsService;
+
+    @PostConstruct
+    public void init(){
+        analyticsService = new AnalyticsService();
     }
 
+    @PostMapping("/analytics")
+    public void getStatsForRoom(){
+        analyticsService.calculateStatistics();
+    }
 }
--- src/main/java/de/unistuttgart/hackathon/taser/AnalyticsService/AnalyticsService.java
+++ src/main/java/de/unistuttgart/hackathon/taser/AnalyticsService/AnalyticsService.java
@@ -1,6 +1,21 @@
 package de.unistuttgart.hackathon.taser.AnalyticsService;
 
+import org.apache.tomcat.jni.Local;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClient;
+import redis.clients.jedis.JedisPooled;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+
 public class AnalyticsService {
+
+    private final Logger logger = LoggerFactory.getLogger(AnalyticsService.class);
+    private final JedisPooled jedisPool = new JedisPooled("localhost", 6379);
+    private final WebClient webClient = WebClient.create("http://localhost:8090");
 
     /**
      * Calculates the Statistics for a specific room
@@ -8,6 +23,35 @@ public class AnalyticsService {
      * Implemented as a cronjob
      */
     public void calculateStatistics(){
+        final Map<String, Queue<Map<LocalDateTime, Boolean>>> queues = getQueueData();
+        for (final Map.Entry<String, Queue<Map<LocalDateTime, Boolean>>> map : queues.entrySet() ){
+            final String identifier = map.getKey();
+            final Queue<Map<LocalDateTime, Boolean>> queue = map.getValue();
+            final List<Float> values = calculateValues(queue);
+            jedisPool.sadd(identifier, values.toString());
+        }
+    }
 
+    /**
+     * Calculates list of
+     * @param queue to calculate the values for
+     * @return
+     */
+    private List<Float> calculateValues(final Queue<Map<LocalDateTime, Boolean>> queue) {
+        final List<Float> values = new ArrayList<>(Collections.nCopies(12, 0f));
+        long now = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        for (final Map<LocalDateTime, Boolean> map : queue){
+            long voteTime = map.entrySet().iterator().next().getKey().toEpochSecond(ZoneOffset.UTC);
+            long diff = now - voteTime;
+            if (map.entrySet().iterator().next().getValue() && (diff <= 120 )){
+                int index = (int)diff/10;
+                values.set(1, values.get(1) + 1);
+            }
+        }
+        return values;
+    }
+
+    private Map<String, Queue<Map<LocalDateTime, Boolean>>> getQueueData() {
+        return webClient.get().uri("/queue/getQueues").retrieve().bodyToMono(new ParameterizedTypeReference<Map<String, Queue<Map<LocalDateTime, Boolean>>>>() {}).block();
     }
 }
